0f1c6ef069b29833d000fac55a095548bf478486
Jia Yao <s439765@NucleusC097.cm.cluster>
2024-06-01 16:44:23 -0500

    update svs_reader

--- svs-reader/README.md
+++ svs-reader/README.md
@@ -1,10 +1,10 @@
 # svs-reader
 
-[![License BSD-3](https://img.shields.io/pypi/l/svs-reader.svg?color=green)](https://github.com/yaber/svs-reader/raw/main/LICENSE)
+[![License MIT](https://img.shields.io/pypi/l/svs-reader.svg?color=green)](https://github.com/yaober/svs-reader/raw/main/LICENSE)
 [![PyPI](https://img.shields.io/pypi/v/svs-reader.svg?color=green)](https://pypi.org/project/svs-reader)
 [![Python Version](https://img.shields.io/pypi/pyversions/svs-reader.svg?color=green)](https://python.org)
-[![tests](https://github.com/yaber/svs-reader/workflows/tests/badge.svg)](https://github.com/yaber/svs-reader/actions)
-[![codecov](https://codecov.io/gh/yaber/svs-reader/branch/main/graph/badge.svg)](https://codecov.io/gh/yaber/svs-reader)
+[![tests](https://github.com/yaober/svs-reader/workflows/tests/badge.svg)](https://github.com/yaober/svs-reader/actions)
+[![codecov](https://codecov.io/gh/yaober/svs-reader/branch/main/graph/badge.svg)](https://codecov.io/gh/yaober/svs-reader)
 [![napari hub](https://img.shields.io/endpoint?url=https://api.napari-hub.org/shields/svs-reader)](https://napari-hub.org/plugins/svs-reader)
 
 A Napari plugin to read SVS files
@@ -37,7 +37,7 @@ the coverage at least stays the same before you submit a pull request.
 
 ## License
 
-Distributed under the terms of the [BSD-3] license,
+Distributed under the terms of the [MIT] license,
 "svs-reader" is free and open source software
 
 ## Issues
--- svs-reader/src/svs_reader/_reader.py
+++ svs-reader/src/svs_reader/_reader.py
@@ -1,38 +1,86 @@
-# src/svs_reader/_reader.py
+"""
+This module is an example of a barebones numpy reader plugin for napari.
+
+It implements the Reader specification, but your plugin may choose to
+implement multiple readers or even other plugin contributions. see:
+https://napari.org/stable/plugins/guides.html?#readers
+"""
 
 import numpy as np
 import openslide
 
-def svs_reader_function(path):
-    """
-    Read an SVS file and return a list of numpy arrays representing the image pyramid.
-    
+
+def napari_get_reader(path):
+    """A basic implementation of a Reader contribution.
+
     Parameters
     ----------
-    path : str
-        Path to the SVS file.
+    path : str or list of str
+        Path to file, or list of paths.
 
     Returns
     -------
-    data : list of numpy.ndarray
-        List of numpy arrays, each representing a different level of the image pyramid.
+    function or None
+        If the path is a recognized format, return a function that accepts the
+        same path or list of paths, and returns a list of layer data tuples.
+    """
+    if isinstance(path, list):
+        # A path list does not make sense for this plugin
+        return None
+
+    # otherwise we return the *function* that can read ``path``.
+    if path.endswith(".svs"):
+        return reader_function
+
+    # otherwise we return the *function* that can read ``path``.
+    return None
+
+
+def reader_function(path):
+    """Take a path or list of paths and return a list of LayerData tuples.
+
+    Readers are expected to return data as a list of tuples, where each tuple
+    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
+    both optional.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    layer_data : list of tuples
+        A list of LayerData tuples where each tuple in the list contains
+        (data, metadata, layer_type), where data is a numpy array, metadata is
+        a dict of keyword arguments for the corresponding viewer.add_* method
+        in napari, and layer_type is a lower-case string naming the type of
+        layer. Both "meta", and "layer_type" are optional. napari will
+        default to layer_type=="image" if not provided
     """
     slide = openslide.OpenSlide(path)
-    myPyramid = []
+    num_levels = slide.level_count
 
-    for level in range(slide.level_count):
-        dims = slide.level_dimensions[level]
-        img = slide.read_region((0, 0), level, dims)
-        img_np = np.array(img)
-        myPyramid.append(img_np)
+    myPyramid = []
+    # Loop through each level and read the image data
+    for level in range(num_levels):
+        # Get dimensions for the current level
+        level_dimensions = slide.level_dimensions[level]
+        print(f"Reading Level {level} with dimensions: {level_dimensions}")
+
+        # Read the entire region for the current level
+        region = slide.read_region((0, 0), level, level_dimensions)
+
+        # Convert the region to a numpy array
+        region_array = np.array(region)
+
+        # Append the numpy array to the list
+        myPyramid.append(region_array)
 
     slide.close()
-    return myPyramid
-
-# You may need to adjust the function signature to match the expected reader signature
-def napari_get_reader(path):
-    if isinstance(path, list):
-        path = path[0]
-    if not path.endswith('.svs'):
-        return None
-    return svs_reader_function
+    add_kwargs = {
+        "multiscale": True,
+        "contrast_limits": [0, 255],
+    }
+    layer_type = "image"  # optional, default is "image"
+    return [(myPyramid, add_kwargs, layer_type)]
